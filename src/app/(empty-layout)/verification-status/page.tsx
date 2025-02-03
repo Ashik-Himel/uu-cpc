1440bf00f7be61b0734e179040922dd0bc7f96dc
Ashik-Himel <ashikujjamanhimel@gmail.com>
2025-02-03 22:22:40 +0600

    back to home button added in the verification status page

--- src/app/(empty-layout)/verification-status/page.tsx
+++ src/app/(empty-layout)/verification-status/page.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { Metadata } from "next";
+import Link from "next/link";
+import { Suspense } from "react";
 import Status from "./status";
 
 export const metadata: Metadata = {
@@ -10,8 +13,13 @@ export const metadata: Metadata = {
 export default function VerificationStatus() {
   return (
     <main className="m-6 md:m-10">
-      <div className="bg-primary/5 text-primary font-medium rounded-lg border border-primary px-4 py-8 w-full max-w-[450px] mx-auto text-center">
-        <Status />
+      <div className="bg-primary/5 text-primary font-medium rounded-lg border border-primary px-4 py-6 w-full max-w-[450px] mx-auto text-center">
+        <Suspense fallback={<p>Loading status...</p>}>
+          <Status />
+        </Suspense>
+        <Button className="mt-3" size="sm" asChild>
+          <Link href="/">Back to Home</Link>
+        </Button>
       </div>
     </main>
   );
